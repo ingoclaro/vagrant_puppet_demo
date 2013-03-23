2009b4b891ab2b616b76735a9ddeb97e7dc0467b
Ingo Claro <ingo@groupon.com>
2013-03-23 09:41:42 -0500

    /etc/hosts and firewall fixes

--- puppet/vagrant-manifests/nodes.pp
+++ puppet/vagrant-manifests/nodes.pp
@@ -2,6 +2,35 @@ node 'basenode' {
   include epel
   include atomic
   include repoforge
+
+  file { '/etc/hosts':
+    ensure => present,
+    content => '',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+
+  host { 'localhost4':
+    ensure       => present,
+    ip           => '127.0.0.1',
+    host_aliases => ['localhost.localdomain', 'localhost4', 'localhost4.localdomain4'],
+    subscribe => File['/etc/hosts'],
+  }
+  host { 'localhost6':
+    ensure       => present,
+    ip           => '::1',
+    host_aliases => ['localhost.localdomain', 'localhost6', 'localhost6.localdomain6'],
+    subscribe => File['/etc/hosts'],
+  }
+
+  host { 'hostname':
+    ensure       => present,
+    name         => $hostname,
+    ip           => $ipaddress_eth1,
+    subscribe => File['/etc/hosts'],
+  }
+
 }
 
 node 'lb' inherits basenode {
--- puppet/vagrant-manifests/site.pp
+++ puppet/vagrant-manifests/site.pp
@@ -5,3 +5,37 @@ filebucket { main: server => puppet }
 # global defaults
 File { backup => main }
 Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin" }
+
+
+# Always persist firewall rules
+exec { 'persist-firewall':
+  command     => $operatingsystem ? {
+    'debian'          => '/sbin/iptables-save > /etc/iptables/rules.v4',
+    /(RedHat|CentOS)/ => '/sbin/iptables-save > /etc/sysconfig/iptables',
+  },
+  refreshonly => true,
+}
+
+Firewall {
+  notify  => Exec["persist-firewall"],
+  before  => Class['my_fw::post'],
+  require => Class['my_fw::pre'],
+}
+Firewallchain {
+  notify  => Exec['persist-firewall'],
+}
+
+# Purge unmanaged firewall resources
+#
+# This will clear any existing rules, and make sure that only rules
+# defined in puppet exist on the machine
+resources { "firewall":
+  purge => true
+}
+
+# firewall { '100 accept ssh access':
+#   proto   => 'tcp',
+#   dport   => '22',
+#   state   => ['NEW'],
+#   action  => 'accept',
+# }
